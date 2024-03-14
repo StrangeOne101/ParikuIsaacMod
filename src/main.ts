3603c38a55508939cc41af1a69d7d62c2bc59c47
StrangeOne101 <strange.toby@gmail.com>
2024-03-14 23:25:12 +1300

    Added initial stats for character
    - Updated the version of isaacscript-common to fix range also not being updated, due to a bug in the library

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,32 @@
 import { name } from "../package.json";
-import { upgradeMod  } from "isaacscript-common";
-import type {ModUpgraded} from "isaacscript-common";
+import { upgradeMod, ISCFeature } from "isaacscript-common";
 import * as statTracker from "./stat_tracker";
 import * as costumes from "./costumes";
+import { PARIKU_TYPE } from "./constants";
+import { CacheFlag } from "isaac-typescript-definitions";
 
 
 // This function is run when your mod first initializes.
 export function main(): void {
     // Register the mod object, and then register the upgraded mod object from IsaacScript.
     const modVanilla = RegisterMod(name, 1);
-    const mod: ModUpgraded = upgradeMod(modVanilla);
+
+    const features = [ISCFeature.CHARACTER_STATS] as const;
+    const mod = upgradeMod(modVanilla, features);
 
     // Initialize our mod callbacks in the different components of this mod.
     Isaac.DebugString("Initializing Pariku Costumes...")
     costumes.init(mod);
     Isaac.DebugString("Initializing Pariku Stat Tracker...")
     statTracker.init(mod);
+
+
+    const parikuDefaultStats = new Map<CacheFlag, number>([
+        [CacheFlag.DAMAGE, 3],
+        [CacheFlag.SPEED, 0.9],
+        [CacheFlag.RANGE, 5 * 40]
+      ]);
+    mod.registerCharacterStats(PARIKU_TYPE, parikuDefaultStats);
 
     // Print a message to the "log.txt" file.
     Isaac.DebugString(`${name} initialized.`);
